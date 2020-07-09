2ac3efcb806547e3905d3eb2212c3295c52f6f23
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2020-07-09 16:10:53 +0530

    solved without using division operation

--- DCP-2/solution.java
+++ DCP-2/solution.java
@@ -1,19 +1,21 @@
 import java.util.*;
 
 class Solution {
-
     public int[] solve(int[] nums) {
-    
-        int product = 1;
-        for(int i: nums){
-            product *= i;
-        }
-        
         int[] arr = new int[nums.length];
-        for(int i=0; i<arr.length; i++){
-            arr[i] = product/nums[i];
+        Arrays.fill(arr, 0, arr.length, 1);
+        int leftProduct = 1;
+        int rightProduct = 1;
+
+        for(int i = 0; i < nums.length; i++){
+            arr[i] *= leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        for(int j = nums.length - 1; j >= 0; j--){
+            arr[j] *= rightProduct;
+            rightProduct *= nums[j];
         }
         return arr;
-        
     }
 }
