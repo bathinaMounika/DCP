c3d4adeeeee0baa0b415e75574ffbb6c460c04ed
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2021-09-30 15:06:25 +0530

    Update RandomNumberGenerator.java

--- DCP-403/RandomNumberGenerator.java
+++ DCP-403/RandomNumberGenerator.java
@@ -2,22 +2,22 @@ import java.util.Random;
 
 public class RandomNumberGenerator{
   public static void main(String[] args) {
-    try{
+    try {
       Integer n = Integer.parseInt(args[0]);
       for(int i =0; i < n; i++){
         System.out.println(rand7());
       }
-    }catch(NumberFormatException | ArrayIndexOutOfBoundsException e){
+    } catch(NumberFormatException | ArrayIndexOutOfBoundsException e) {
       System.out.println("Please add valid number in the cli args");
     }
   }
 
-  public static int rand5(){
+  public static int rand5() {
     Random random = new Random();
     return random.nextInt(5) + 1;
   }
 
-  public static int rand7(){
+  public static int rand7() {
     int r = 5 * (rand5() - 1) + (rand5() - 1);
     //rejection Sampling
     if(r < 21) return (r % 7) + 1;
